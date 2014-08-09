2da1de854d5b9731b1808dab3b9c83cf5b07dcbb
Adam Presley <adam@adampresley.com>
2014-08-09 11:36:09 -0500

    Added environment variables for server port and address

--- start.bat
+++ start.bat
@@ -1,5 +1,8 @@
 @echo off
 
+set TEXO_SERVER_ADDRESS=localhost
+set TEXO_SERVER_PORT=8081
+
 set TEXO_POSTS_PER_PAGE=10
 
 set TEXO_SQL_ADDRESS=localhost
@@ -8,4 +11,4 @@ set TEXO_SQL_DATABASE=texocms
 set TEXO_SQL_USERNAME=root
 set TEXO_SQL_PASSWORD=password
 
-go run texo-services.go
+go run texo-services.go -serveraddress=%TEXO_SERVER_ADDRESS% -serverport=%TEXO_SERVER_PORT%
--- start.sh
+++ start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+export TEXO_SERVER_ADDRESS=localhost
+export TEXO_SERVER_PORT=8081
+
+export TEXO_POSTS_PER_PAGE=10
+
 export TEXO_SQL_ADDRESS=localhost
 export TEXO_SQL_PORT=3306
 export TEXO_SQL_DATABASE=texocms
 export TEXO_SQL_USERNAME=root
 export TEXO_SQL_PASSWORD=password
 
-go run ./texo-services.go
+go run ./texo-services.go -serveraddress=$TEXO_SERVER_ADDRESS -serverport=$TEXO_SERVER_PORT
